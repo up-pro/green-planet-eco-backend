6ae9ff14a9005c1c27441a3b8bce7e822f990bcb
mr-new-0509 <cliffahleongtech@gmail.com>
2023-04-26 15:28:42 +0900

    update the admin email into the test

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,5 +1,6 @@
 export const DB_PORT = 3306;
 
-export const ADMIN_EMAIL = "info@greenplaneteco.com";
+// export const ADMIN_EMAIL = "info@greenplaneteco.com";
+export const ADMIN_EMAIL = "mr.new0509@gmail.com";
 export const MAIL_TITLE_OF_CONTACT_US = "Contact Us";
 export const MAIL_TITLE_OF_QUESTION = "Question";
