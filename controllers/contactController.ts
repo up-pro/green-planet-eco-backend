daaebaf1e17c8542d072baa4682d4dc9bcd6be7a
mr-new-0509 <cliffahleongtech@gmail.com>
2023-04-26 16:34:40 +0900

    fix the problem

--- controllers/contactController.ts
+++ controllers/contactController.ts
@@ -4,7 +4,7 @@ import {
   MAIL_TITLE_OF_CONTACT_US,
   MAIL_TITLE_OF_QUESTION,
 } from "../utils/constants";
-
+const SibApiV3Sdk = require("sib-api-v3-sdk");
 const Sib = require("../utils/Sib");
 
 /** Contact Us */
@@ -16,23 +16,34 @@ export const contactToAdmin = (req: Request, res: Response) => {
     return res.sendStatus(400);
   }
 
+  let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
   const tranEmailApi = new Sib.TransactionalEmailsApi();
 
-  const sender = { email };
+  const sender = { email, name };
   const receivers = [{ email: ADMIN_EMAIL }];
-  const mailOptions = {
-    sender,
-    to: receivers,
-    subject: MAIL_TITLE_OF_CONTACT_US,
-    htmlContent: `
-      <h1>${name}</h1>
-      <a href="${website}" target="_blank">${website}</a>
-      <p>${message}</p>
-    `,
-  };
+
+  sendSmtpEmail.subject = MAIL_TITLE_OF_CONTACT_US;
+  sendSmtpEmail.sender = sender;
+  sendSmtpEmail.to = receivers;
+  sendSmtpEmail.htmlContent = `
+    <h1>${name}</h1>
+    <a href="${website}" target="_blank">${website}</a>
+    <p>${message}</p>
+  `;
+
+  // const mailOptions = {
+  //   sender,
+  //   to: receivers,
+  //   subject: MAIL_TITLE_OF_CONTACT_US,
+  //   htmlContent: `
+  //     <h1>${name}</h1>
+  //     <a href="${website}" target="_blank">${website}</a>
+  //     <p>${message}</p>
+  //   `,
+  // };
 
   tranEmailApi
-    .sendTransacEmail(mailOptions)
+    .sendTransacEmail(sendSmtpEmail)
     .then((result: any) => {
       console.log("# result => ", result);
       return res.sendStatus(200);
@@ -52,22 +63,31 @@ export const askQuestion = (req: Request, res: Response) => {
     return res.sendStatus(400);
   }
 
+  let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
   const tranEmailApi = new Sib.TransactionalEmailsApi();
 
-  const sender = { email };
+  const sender = { email, name };
   const receivers = [{ email: ADMIN_EMAIL }];
-  const mailOptions = {
-    sender,
-    to: receivers,
-    subject: MAIL_TITLE_OF_QUESTION,
-    htmlContent: `
-      <h1>${name}</h1>
-      <p>${message}</p>
-    `,
-  };
+
+  sendSmtpEmail.subject = MAIL_TITLE_OF_QUESTION;
+  sendSmtpEmail.sender = sender;
+  sendSmtpEmail.to = receivers;
+  sendSmtpEmail.htmlContent = `
+    <h1>${name}</h1>
+    <p>${message}</p>
+  `;
+  // const mailOptions = {
+  //   sender,
+  //   to: receivers,
+  //   subject: MAIL_TITLE_OF_QUESTION,
+  //   htmlContent: `
+  //     <h1>${name}</h1>
+  //     <p>${message}</p>
+  //   `,
+  // };
 
   tranEmailApi
-    .sendTransacEmail(mailOptions)
+    .sendTransacEmail(sendSmtpEmail)
     .then((result: any) => {
       console.log("# result => ", result);
       return res.sendStatus(200);
