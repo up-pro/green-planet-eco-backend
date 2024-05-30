696cf26881b112bb3db2d0b912e8f1436591a813
mavsin <koizatsuo@gmail.com>
2024-05-31 07:57:00 +0900

    chore: remove db password

--- utils/sequelize.ts
+++ utils/sequelize.ts
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 
 const DB_NAME = process.env.DB_NAME || "";
 const DB_USERNAME = process.env.DB_USERNAME || "";
-const DB_PASSWORD = process.env.DB_PASSWORD || "";
+const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_HOST = process.env.DB_HOST || "";
 
 console.log('>>>>>>>> DB credentials', {
