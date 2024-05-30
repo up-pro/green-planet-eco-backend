423c6deed4b58ee187495eb13731b90cc8f4e8d7
mavsin <koizatsuo@gmail.com>
2024-05-30 10:35:45 +0900

    chore: console DB credentials

--- utils/sequelize.ts
+++ utils/sequelize.ts
@@ -5,6 +5,13 @@ const DB_USERNAME = process.env.DB_USERNAME || "";
 const DB_PASSWORD = process.env.DB_PASSWORD || "";
 const DB_HOST = process.env.DB_HOST || "";
 
+console.log('>>>>>>>> DB credentials', {
+  DB_NAME,
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_HOST,
+})
+
 const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
   host: DB_HOST,
   dialect: "mysql",
